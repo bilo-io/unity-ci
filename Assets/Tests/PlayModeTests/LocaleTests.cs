0c51843dac4295c6d77a49584bd45425d0f0150b
Bilo Lwabona <bilo.lwabona@gmail.com>
2020-09-16 01:07:14 +0200

    added some unity PlayMode & EditMode tests

--- Assets/Tests/PlayModeTests/LocaleTests.cs
+++ Assets/Tests/PlayModeTests/LocaleTests.cs
@@ -6,11 +6,11 @@ using UnityEngine.TestTools;
 
 namespace Tests
 {
-    public class Startup
+    public class LocaleTests
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void StartupSimplePasses()
+        public void LocaleTestsSimplePasses()
         {
             // Use the Assert class to test conditions
         }
@@ -18,24 +18,11 @@ namespace Tests
         // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // `yield return null;` to skip a frame.
         [UnityTest]
-        public IEnumerator StartupWithEnumeratorPasses()
+        public IEnumerator LocaleTestsWithEnumeratorPasses()
         {
             // Use the Assert class to test conditions.
             // Use yield to skip a frame.
             yield return null;
         }
-
-        [UnityTest]
-        public IEnumerator GameObject_WithRigidBody_WillBeAffectedByPhysics()
-        {
-            var go = new GameObject();
-            go.AddComponent<Rigidbody>();
-            var originalPosition = go.transform.position.y;
-
-            yield return new WaitForFixedUpdate();
-
-            Assert.AreNotEqual(originalPosition, go.transform.position.y);
-        }
-
     }
 }
