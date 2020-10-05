14c64c4a2189570eabde0608e1298a47babec749
Bilo Lwabona <bilo.lwabona@gmail.com>
2020-10-05 15:37:42 +0200

    added isHttpError check

--- Assets/App/Scripts/API/Adapters/TodoAdapter.cs
+++ Assets/App/Scripts/API/Adapters/TodoAdapter.cs
@@ -32,12 +32,12 @@ public class TodoAdapter : MonoBehaviour
             HandleResponse(request);
         }), null));
 
-        StartCoroutine(RestClientWebGL.PutAsync<Todo>("todos/5f7b09a3444eed2f702e09e6", jsonData, null, (request =>
+        StartCoroutine(RestClientWebGL.PutAsync<Todo>("todos/5f5a851a680a3f4c81f44258", jsonData, null, (request =>
         {
             HandleResponse(request);
         }), null));
 
-        StartCoroutine(RestClientWebGL.DeleteAsync<object>("todos/5f7b09a3444eed2f702e09e6", null, (request =>
+        StartCoroutine(RestClientWebGL.DeleteAsync<object>("todos/5f5a851a680a3f4c81f44258", null, (request =>
         {
             HandleResponse(request);
         }), null));
@@ -45,20 +45,21 @@ public class TodoAdapter : MonoBehaviour
 
     void HandleResponse(UnityWebRequest request)
     {
-        if (request.isNetworkError)
+        string requestSummary = $"{request.method} ({request.responseCode})";
+        if (request.isNetworkError || request.isHttpError)
         {
-            Debug.Log($"<color=red>({request.responseCode}) => API.ERROR</color>");
+            Debug.Log($"<color=red>{requestSummary} => API.ERROR</color>");
         }
         else
         {
             if (request.responseCode < 400)
             {
                 var json = request?.downloadHandler?.text;
-                Debug.Log($"<color=#44FF90>({request.responseCode}) => API.SUCCESS</color>\n{json}");
+                Debug.Log($"<color=#44FF90>{requestSummary} => API.SUCCESS</color>\n{json}");
             }
             else
             {
-                Debug.Log($"<color=red>({request.responseCode}) => API.ERROR</color> {request.error}");
+                Debug.Log($"<color=red>{requestSummary} => API.ERROR</color> {request.error}");
             }
         }
     }
